b9a249091ba1f271d631ca065f7972e580c37f06
Clarence "Sparr" Risher <sparr0@gmail.com>
2020-09-06 18:50:04 -0700

    Prevent mixing fluids, v1.0.2

--- control.lua
+++ control.lua
@@ -1,57 +1,78 @@
-local neighbor_lookup = {
-    [defines.direction.north] = { neighbors={
-            {pos={-1,-1},dir=defines.direction.east },
-            {pos={ 0,-2},dir=defines.direction.south},
-            {pos={ 1,-1},dir=defines.direction.west },
-        }, connect={ 0,-1} },
-    [defines.direction.east ] = { neighbors={
-            {pos={ 1,-1},dir=defines.direction.south},
-            {pos={ 2, 0},dir=defines.direction.west },
-            {pos={ 1, 1},dir=defines.direction.north},
-        }, connect={ 1, 0} },
-    [defines.direction.south] = { neighbors={
-            {pos={ 1, 1},dir=defines.direction.west },
-            {pos={ 0, 2},dir=defines.direction.north},
-            {pos={-1, 1},dir=defines.direction.east },
-        }, connect={ 0, 1} },
-    [defines.direction.west ] = { neighbors={
-            {pos={-1, 1},dir=defines.direction.north},
-            {pos={-2, 0},dir=defines.direction.east },
-            {pos={-1,-1},dir=defines.direction.south},
-        }, connect={-1, 0} },
+-- underground pipe neighbor lookup table
+-- keys are underground pipe directions
+-- values describe the possible relative locations and directions of another to connect to
+local direction_to_neighbors = {
+    [defines.direction.north] = { -- for an underground pipe pointing north
+        {pos={-1,-1}, dir=defines.direction.east }, -- one space ahead and left
+        {pos={ 0,-2}, dir=defines.direction.south}, -- two spaces ahead
+        {pos={ 1,-1}, dir=defines.direction.west }, -- one space ahead and right
+    },
+    [defines.direction.east ] = {
+        {pos={ 1,-1}, dir=defines.direction.south},
+        {pos={ 2, 0}, dir=defines.direction.west },
+        {pos={ 1, 1}, dir=defines.direction.north},
+    },
+    [defines.direction.south] = {
+        {pos={ 1, 1}, dir=defines.direction.west },
+        {pos={ 0, 2}, dir=defines.direction.north},
+        {pos={-1, 1}, dir=defines.direction.east },
+    },
+    [defines.direction.west ] = {
+        {pos={-1, 1}, dir=defines.direction.north},
+        {pos={-2, 0}, dir=defines.direction.east },
+        {pos={-1,-1}, dir=defines.direction.south},
+    },
+}
+
+-- x and y offsets to move one step in the given direction
+local direction_to_delta = {
+    [defines.direction.north] = { 0, -1},
+    [defines.direction.east ] = { 1,  0},
+    [defines.direction.south] = { 0,  1},
+    [defines.direction.west ] = {-1,  0},
 }
 
 function on_built_entity(event)
+    -- locals for efficient repeat access
     local underground_entity = event.created_entity
     local underground_name = underground_entity.name
     local underground_direction = underground_entity.direction
     local underground_position = underground_entity.position
+    local neighbor_info = direction_to_neighbors[underground_direction]
+    local pipe_lookup = global.pipe_lookup[underground_name]
+    local pipe_position = direction_to_delta[underground_direction]
 
-    -- bail out if there's already something where we would put a pipe
-    local connect = neighbor_lookup[underground_direction].connect
-    local blocking_entity = underground_entity.surface.find_entities(
-        {{underground_position.x + connect[1] - 0.5, underground_position.y + connect[2] - 0.5},
-         {underground_position.x + connect[1] + 0.5, underground_position.y + connect[2] + 0.5}}
-    )
-    if #blocking_entity > 0 then return end
+    local pipe_entity_definition = {
+        name = pipe_lookup[2],
+        position = {underground_position.x + pipe_position[1], underground_position.y + pipe_position[2]},
+
+        -- properties just for create_entity
+        force = underground_entity.force,
+        last_user = underground_entity.last_user,
+        raise_built = true,
+        create_build_effect_smoke = true,
+        spawn_decorations = true,
+
+        -- properties just for can_place_entity
+        build_check_type = defines.build_check_type.manual,
+    }
+
+    -- bail out if we can't place a pipe, could be blocked or a fluid mixing violation
+    if not underground_entity.surface.can_place_entity(pipe_entity_definition) then return end
+
     -- look at the three possible locations for another underground to connect to
-    local neighbor_candidates = neighbor_lookup[underground_direction].neighbors
-    for _, neighbor_candidate in pairs(neighbor_candidates) do
+    for _, neighbor_candidate in pairs(neighbor_info) do
         local neighbor_entity = underground_entity.surface.find_entity(
             underground_name,
             {underground_position.x + neighbor_candidate.pos[1], underground_position.y + neighbor_candidate.pos[2]}
         )
         if neighbor_entity and neighbor_entity.direction == neighbor_candidate.dir then
-            local lookup = global.pipe_lookup[underground_name]
             -- found one in the right place and direction
-            if game.players[event.player_index].get_main_inventory().find_item_stack(lookup[1]) then
+            if game.players[event.player_index].get_main_inventory().find_item_stack(pipe_lookup[1]) then
                 -- we have a pipe in inventory, so spend it
-                game.players[event.player_index].get_main_inventory().remove({name=lookup[1]})
+                game.players[event.player_index].get_main_inventory().remove({name=pipe_lookup[1]})
                 -- to place the pipe entity
-                underground_entity.surface.create_entity{
-                    name = lookup[2],
-                    position = {underground_position.x + connect[1], underground_position.y + connect[2]}
-                }
+                underground_entity.surface.create_entity(pipe_entity_definition)
             end
             break
         end
@@ -63,7 +84,7 @@ global.pipe_lookup = global.pipe_lookup or {}
 -- Find recipes that produce underground pipes and try to figure out which pipe they belong with
 function rebuild_index()
     local underground_recipe_prototypes = game.get_filtered_recipe_prototypes({{filter="has-product-item",elem_filters={{filter="place-result",elem_filters={{filter="type",type="pipe-to-ground"}}}}},{filter="has-ingredient-item",mode="and",elem_filters={{filter="place-result",elem_filters={{filter="type",type="pipe"}}}}}})
-    for urp_name, urp_prototype in pairs(underground_recipe_prototypes) do
+    for _, urp_prototype in pairs(underground_recipe_prototypes) do
         local underground_entity_name, pipe_item_name, pipe_entity_name
         for _, product in pairs(urp_prototype.products) do
             if game.entity_prototypes[game.item_prototypes[product.name].place_result.name].type == "pipe-to-ground" then
